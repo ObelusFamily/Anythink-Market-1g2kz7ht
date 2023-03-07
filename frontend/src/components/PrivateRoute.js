d2b07ee5525cb172cc0270a9edbdd7de8696b705
Kiryl Fando <kirillfando@gmail.com>
2023-03-08 00:31:43 +0100

    fix: add useMemo to avoid infinite rerenders

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,22 +1,22 @@
 import { Route, Redirect } from "react-router-dom";
+import { useMemo } from "react";
 
 const PrivateRoute = ({ component: Component, currentUser }) => {
-  return (
-    <Route
-      render={props => (currentUser ? (
+  const renderFunction = useMemo(() => {
+    return (props) =>
+      currentUser ? (
         <Component {...props} />
       ) : (
         <Redirect
           to={{
             pathname: "/",
-            state: { from: props.location }
+            state: { from: props.location },
           }}
         />
-      )
-      )
-      }
-    />
-  );
+      );
+  }, [Component, currentUser]);
+
+  return <Route render={renderFunction} />;
 };
 
 export default PrivateRoute;
