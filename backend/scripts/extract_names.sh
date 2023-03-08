aa010c9642ebadcdb3738d88f90c01aacfd5581a
Kiryl Fando <kirillfando@gmail.com>
2023-03-08 01:24:15 +0100

    fix: fix script for work with local files

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Check that a URL argument was provided
+# Check that a filename argument was provided
 if [ -z "$1" ]; then
-  echo "Please provide a Google Sheets URL as the first argument"
+  echo "Please provide a filename as the first argument"
   exit 1
 fi
 
-# Set the Google Sheets API endpoint using the provided URL
-SHEET_URL="$1/export?format=csv"
-
-# Download the sheet data and filter for the desired emails using grep
-curl -sSL "$SHEET_URL" | grep -i '@amazon.com' | awk -F',' '{print $3, $2}' > output.txt
+# Filter for the desired emails using grep (case-insensitive), then swap the first and last name fields using awk
+cat "$1" | grep -i '@amazon.com' | awk -F',' '{print $3, $2}' > output_names.txt
